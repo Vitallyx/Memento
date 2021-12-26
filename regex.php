36753a5e5ceda94391394855ac0be754b0a33aa1
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-12-26 15:01:14 +0100

    1

--- regex.php
+++ regex.php
@@ -45,36 +45,39 @@ Veuillez lire les notes pour mieux comprendre la structure du mémento
 
 
 
- Les Métacaractère :
+ Les Métacaractère & Ancres :
 
-╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║► Tag : #^
-╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-║ Inséré dans une classe, le caractère ^ indique une exception pour la recherche 
+╠════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║Inséré dans une classe, le caractère ^ indique une exception pour la recherche 
 ║   [^n]  tout caractère, sauf n  ←−→ (fzaff)n(gez)n(fez)
 ║
-║ En dehors d'une classe elle établit une correspondance avec des caractères au début d’une chaîne de caractères 
+║En dehors d'une classe elle établit une correspondance avec des caractères au début d’une chaîne de caractères 
 ║   ^https  commence par https ←−→ (https):test.com ; (https)fzafaz ; www.test.com
 ║
 ║Remarque : 
 ║► Seulement ^ est actifs dans une classe, les autres métacaractères ne sont considérés que comme simples caractères
 ║
-╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║► Tag : #$
-╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-║ Établit une correspondance avec des caractères à la fin d’une chaîne de caractères
-║   abc$  termine par abc ←−→ fzaf(abc) ; fzafabh
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║Établit une correspondance avec des caractères à la fin d’une chaîne de caractères
+║   val$  termine par val ←−→ fzaf(val) ; fzafabh
 ║   [xyz]$  termine par x, y ou z ←−→ faghex(z) ; fafzae(y) ; gezgzh 
 ║
-╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-║► Tag : #\S, #\s, #\w, #\W, #\D, #\d, #\a, #\b, #\B
-╠══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\S, #\s 
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║\S	Correspond à tous les caractères qui ne sont pas des caractères d’espacement
 ║   Va\Slwyns  Va suivi de tous caractères hors caractères d’espacement ←−→ (VaXlwyns)fz ; fza(Va1lwyns)s ; Va lwyns
 ║
 ║\s Correspond à tout caractère d’espacement (espace, tabulation, saut de ligne ou saut de page)
 ║   Valwyns\sdev  Valwyns suivi d'un caractère d’espacement puis dev ←−→ Valwyns dev ; (Valwyns[tab]dev)dz ; Valwyns<2 espaces>dev ; Valwynsdev ; Valwzyns dev
 ║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\w, #\W
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║\w Correspond à n’importe quel caractère alphanumérique
 ║   \w\w\w  sélectionne par bloc de 3 caractères alphanumérique ←−→ (daz) ; (gez)(gze) ; (Ffz)1D ici Ffz est sélectionné mais pas 1D car il n'y a que 2 caractères
 ║   ab\w\w\w  ab suivi de 3 caractères alphanumérique ←−→ faf(abfzd)htr
@@ -82,28 +85,75 @@ Veuillez lire les notes pour mieux comprendre la structure du mémento
 ║\W	Correspond à n’importe quel caractère non alphanumérique
 ║   valwyns\W  valwyns suivi de n’importe quel caractère non alphanumérique ←−→ (valwyns!) ; (valwyns?)
 ║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\D, #\d
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║\d	Correspond à tout chiffre
-║   abc\d\d  abc suivi de 2 chiffres ←−→ (abc80) ; (abc90) ; abc4z ; aev52
+║   val\d\d  val suivi de 2 chiffres ←−→ (val80) ; (val90) ; val4z ; aev52
 ║
 ║\D	Correspond à tout caractère qui n’est pas un chiffre
-║   abc\D  abc suivi d'un caractère qui n’est pas un chiffre ←−→ (abc!) ; (abc-). ; (abce). ; abfaabc1e
+║   val\D  val suivi d'un caractère qui n’est pas un chiffre ←−→ (val!) ; (val-). ; (vale). ; abfaval1e
 ║
-║\a	Correspond à tout caractère alphabétique, majuscule ou minuscule
-║   n\arz  n suivi d'un caractère alphabétique puis de rz ←−→ (nWrz)dz ; (nfrz)fs ; (narz).
-║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\b, #\B
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║\b	Définit une limite de mot
 ║   \bval  aucun caractère avant val ←−→ (val) ; (val)he ; ndzawval.
 ║
 ║\B	Définit une non-limite de mot
 ║   \Bval  au moins 1 caractère avant val ←−→ ktyew(val)fza ; dzad(val)kty ; valgzeg ; f(val)fez
 ║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\a
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║\a	Correspond à tout caractère alphabétique, majuscule ou minuscule
+║   v\aal  v suivi d'un caractère alphabétique puis de al ←−→ (vWal)dz ; (vfal)fs ; (vaal).
+║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\f
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║\f	Correspond à un caractère saut de page
 ║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\n
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ║\n	Correspond à un caractère de retour à la ligne
 ║
-║ 
-╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
- 
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\s, #\S
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║\s	Correspond à tout blanc incluant des espaces, des onglets, des caractères avance page, etc
+║   val\sd  val suivi d'un caractère "blanc" puis de d ←−→ (val d) ; fa(val[tab]d)afa
+║
+║\S	Correspond à tout caractère espace autre qu'un blanc
+║   val\sd  val suivi d'un caractère hors "blanc" puis de d ←−→ (valdd) ; fa(val1d)afa
+║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\t
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║\t	Correspond à un caractère de tabulation
+║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\v, #\V
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║\v	Corresponds à tout blanc vertical
+║
+║\V Corresponds à tous les caractères qui ne sont pas des blancs verticaux
+║
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║► Tag : #\h, #\H
+╠═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+║\h Corresponds à tout blanc honrizontal
+║
+║\H Corresponds à tous les caractères qui ne sont pas des blanc honrizontaux
+║
+╚═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+
+
+
+
+
+
  Exemple de Regex :
 
  [net$] Correspond à n’importe lequel des caractères n, e, t ou $ ($ est un métacaractère, mais dans une classe de caractères, il ne correspond qu’à $).
@@ -118,7 +168,7 @@ Veuillez lire les notes pour mieux comprendre la structure du mémento
 
  net[a-z] correspond à neta, netw et netf etc, mais pas à net1
 
- [abc].$ termine par a, b ou c suivi de n'importe quels caractères ←−→ fzafb1, fzafbh, jjytcg
+ [val].$ termine par a, b ou c suivi de n'importe quels caractères ←−→ fzafb1, fzafbh, jjytcg
 
  ^Val.*Dev$ commence par Val, peut avoir 0 ou plusieurs caractères (n'importe lesquels), doit se terminer par Dev ←−→ Valfza44fas5Dev
 
@@ -128,15 +178,6 @@ Veuillez lire les notes pour mieux comprendre la structure du mémento
 
  A trier
 
- \D Tous les caractères hors chiffre \d Tous les chiffres
- \W Tous les caractères non alphanumérique - \w Tous les caractères alphanumérique
- \S Tous les non séparateur - \s N’importe quel séparateur (espace, tabulation, nouvelle ligne...)
- \V Tous les espaces verticaux - \v Tous les non espaces verticaux
- \H Tous les espaces horizontaux - \h Tous les non espaces horizontaux
- \n Saut de ligne
- \A Début de la chaine
- \z Fin de la chaine
-
  
   [A-Z] Au moins 1 Majuscule
 
@@ -144,9 +185,9 @@ Veuillez lire les notes pour mieux comprendre la structure du mémento
 
  [0-9] Chiffre
 
- [^abc] Tous les caractères sauf a, b ou c
+ [^val] Tous les caractères sauf a, b ou c
 
- [abc] 1 caractère parmi a,b ou c
+ [val] 1 caractère parmi a,b ou c
 
 
  a{x} Exactement x nombre de caractères de a
